4fda2325d1604f2df8178288f958a78de849d2df
Sarmad Chaudhary <sarmad.asif179@gmail.com>
2020-08-14 22:11:04 +0500

    Inital Commit With Starter

---
+++ packages/components/stencil.config.ts
@@ -0,0 +1,92 @@
+import { Config } from '@stencil/core';
+import { angularOutputTarget, ValueAccessorConfig } from '@stencil/angular-output-target';
+import { reactOutputTarget } from '@stencil/react-output-target';
+import { vueOutputTarget, ComponentModelConfig } from '@stencil/vue-output-target';
+
+const angularValueAccessorBindings: ValueAccessorConfig[] = [
+  {
+    elementSelectors: ['my-input[type=text]'],
+    event: 'myChange',
+    targetAttr: 'value',
+    type: 'text',
+  },
+  {
+    elementSelectors: ['my-input[type=number]'],
+    event: 'myChange',
+    targetAttr: 'value',
+    type: 'number',
+  },
+  {
+    elementSelectors: ['my-checkbox'],
+    event: 'myChange',
+    targetAttr: 'checked',
+    type: 'boolean',
+  },
+  {
+    elementSelectors: ['my-radio'],
+    event: 'mySelect',
+    targetAttr: 'checked',
+    type: 'radio',
+  },
+  {
+    elementSelectors: ['my-range', 'my-radio-group'],
+    event: 'myChange',
+    targetAttr: 'value',
+    type: 'select',
+  },
+];
+
+const vueComponentModels: ComponentModelConfig[] = [
+  {
+    elements: ['my-input', 'my-range'],
+    event: 'myChange',
+    targetAttr: 'value',
+  },
+  {
+    elements: ['my-checkbox'],
+    event: 'myChange',
+    targetAttr: 'checked',
+  },
+  {
+    elements: ['my-radio'],
+    event: 'mySelect',
+    targetAttr: 'checked',
+  },
+  {
+    elements: ['my-range', 'my-radio-group'],
+    event: 'myChange',
+    targetAttr: 'value',
+  },
+];
+
+export const config: Config = {
+  namespace: 'component-library',
+  taskQueue: 'async',
+  outputTargets: [
+    angularOutputTarget({
+      componentCorePackage: '@ebiz-kit/components',
+      directivesProxyFile: '../angular/src/directives/proxies.ts',
+      valueAccessorConfigs: angularValueAccessorBindings,
+    }),
+    reactOutputTarget({
+      componentCorePackage: '@ebiz-kit/components',
+      proxiesFile: '../react/src/components.ts',
+    }),
+    vueOutputTarget({
+      componentCorePackage: '@ebiz-kit/components',
+      proxiesFile: '../vue/src/proxies.ts',
+      componentModels: vueComponentModels,
+    }),
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+};
